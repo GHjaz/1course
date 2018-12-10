4d3bd547636845ef755097eb4f8eae86c2f221d5
Nik445 <43647906+GHjaz@users.noreply.github.com>
2018-12-10 23:46:30 +0700

    Minimal long of word in a text

---
+++ 6lapka/6lapka/6lapka.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+	int n,min,k=0;
+	char c[100];
+	gets_s(c);
+	n = strlen(c);
+	c[n + 1] = ' ';
+	min = 100;
+	
+	for (int i = 0; i<strlen(c)+1; i++) {
+		if (c[i] != ' ') {
+			k++;
+		}
+		else { 
+			if ((k < min)&&(k)) {
+				min = k;
+			}
+			k = 0;
+		}
+	}
+	printf("\n%d", min);
+}
